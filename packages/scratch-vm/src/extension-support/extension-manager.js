3df66c5043b69d0f32f3448bd62d5d963d374b9f
Vishesh Kumar <visheshkay@gmail.com>
2023-02-16 01:30:17 -0800

    removing all tfjs packages and extensions

--- packages/scratch-vm/src/extension-support/extension-manager.js
+++ packages/scratch-vm/src/extension-support/extension-manager.js
@@ -31,9 +31,9 @@ const builtinExtensions = {
     microbitRobot: () => require('../extensions/scratch3_microbot'),
     // teachableMachine: () => require('../extensions/scratch3_teachable_machine'),
     // textClassification: () => require('../extensions/scratch3_text_classification'),
-    poseFace: () => require('../extensions/scratch3_pose_face'),
-    poseHand: () => require('../extensions/scratch3_pose_hand'),
-    poseBody: () => require('../extensions/scratch3_pose_body')
+    // poseFace: () => require('../extensions/scratch3_pose_face'),
+    // poseHand: () => require('../extensions/scratch3_pose_hand'),
+    // poseBody: () => require('../extensions/scratch3_pose_body')
 };
 
 /**
